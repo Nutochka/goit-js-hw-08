c107eab819e34ec264749ae18c1ed2263a553519
Anna Shurubei <iamnutochka@gmail.com>
2022-11-04 19:14:53 +0000

    Adds local storage changes and video preferences

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,45 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-console.log(galleryItems)
+import SimpleLightbox from "simplelightbox";
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+console.log(createGallery(galleryItems));
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <a class ="gallery__item" href="${original}">
+            <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}">
+        </a>
+        `;
+    })
+    .join('');
+};
+
+const galleryImage = document.querySelector('.gallery img');
+
+galleryContainer.addEventListener('click', clickOpenImage);
+
+function clickOpenImage(event) {
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+    event.preventDefault()
+}
